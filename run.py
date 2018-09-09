93e994baad10a08a95ccdfc1186fbf992c1b096a
Juan Pablo Balarini <jpbalarini@gmail.com>
2018-09-09 20:40:34 -0300

    Integrating deep sort

--- run.py
+++ run.py
@@ -6,8 +6,21 @@ from PIL import Image
 import glob
 import cv2
 import numpy as np
+import os
+
+from lib.deep_sort.application_util import preprocessing
+from lib.deep_sort.deep_sort import nn_matching
+from lib.deep_sort.deep_sort.detection import Detection
+from lib.deep_sort.deep_sort.tracker import Tracker
+from lib.deep_sort.deep_sort.detection import Detection
+from lib.deep_sort.tools import generate_detections
 
 FLAGS = None
+# deep sort config
+deep_sort_model_filename = 'lib/deep_sort/model_data/mars-small128.pb'
+max_cosine_distance = 0.3
+nn_budget = None
+nms_max_overlap = 1.0
 
 def detect_img(yolo, image_path):
     try:
@@ -15,10 +28,59 @@ def detect_img(yolo, image_path):
     except:
         print('Open Error! Try again!')
     else:
-        r_image = yolo.detect_image(image)
-        opencvImage = cv2.cvtColor(np.array(r_image), cv2.COLOR_RGB2BGR)
-        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-        cv2.imshow("result", opencvImage)
+        r_image, boxes = yolo.detect_image(image)
+        # opencvImage = cv2.cvtColor(np.array(r_image), cv2.COLOR_RGB2BGR)
+        # cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+        # cv2.imshow('result', opencvImage)
+
+        # cv2.imwrite(os.path.splitext(image_path)[0] + '_output.jpg', opencvImage)
+        return r_image, boxes
+
+def deep_sort(frame, detection_boxes, tracker, encoder):
+    features = encoder(frame, detection_boxes)
+    detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(detection_boxes, features)]
+
+    # Run non-maxima suppression.
+    boxes = np.array([d.tlwh for d in detections])
+    scores = np.array([d.confidence for d in detections])
+    indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+    detections = [detections[i] for i in indices]
+
+    # Update tracker.
+    tracker.predict()
+    tracker.update(detections)
+
+    for track in tracker.tracks:
+        if track.is_confirmed() and track.time_since_update > 1:
+            continue
+        bbox = track.to_tlbr()
+        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+        cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+
+    for det in detections:
+        bbox = det.to_tlbr()
+        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+
+    opencvImage = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+    cv2.imshow('result', opencvImage)
+
+
+def main(FLAGS):
+    yolo = YOLO(**vars(FLAGS))
+
+    encoder = generate_detections.create_box_encoder(deep_sort_model_filename, batch_size=1)
+    metric = nn_matching.NearestNeighborDistanceMetric('cosine', max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    for image_path in glob.glob(FLAGS.images_path + '/*.jpg'):
+        image, boxes = detect_img(yolo, image_path)
+        deep_sort(np.array(image), boxes, tracker, encoder)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    yolo.close_session()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
@@ -56,6 +118,7 @@ if __name__ == '__main__':
         help = "[Optional] Video output path"
     )
 
+    # /Volumes/BACKUP_2TB/Maestria/Code/a
     parser.add_argument(
         '--images_path', type=str,
         help='Path from where to read images', default='/Volumes/BACKUP_2TB/Maestria/Datasets/Crowd_PETS09/S2/L1/Time_12-34/View_001'
@@ -63,12 +126,4 @@ if __name__ == '__main__':
 
     FLAGS = parser.parse_args()
 
-    print('Going to run first stage of the pipeline...')
-
-    yolo = YOLO(**vars(FLAGS))
-    for image_path in glob.glob(FLAGS.images_path + '/*.jpg'):
-        detect_img(yolo, image_path)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    yolo.close_session()
-
+    main(FLAGS)
